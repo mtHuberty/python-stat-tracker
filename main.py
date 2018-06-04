88b36ae72a06e4d071ee34a90513850e054065a1
Matt Huberty <matthewhuberty77@gmail.com>
2018-06-03 23:46:16 -0500

    Upgraded backend to connect to PostgresSQL instance. This will break the application until PG installed w/ table.

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import requests as req
 import yaml
 import sys
 import html
+import psycopg2
 
-# Optionally, a user can choose a few options by altering values in this file. File is not required for app to run.
+# OPTIONAL - A user can choose a few options by altering values in this file. File is not required for app to run.
 def loadConfig():
     with open("config.yml", "r") as stream:
         try:
@@ -16,7 +17,7 @@ def loadConfig():
             print("Got a YAML error:\n\t" + str(e))
             return ""
 
-# Optionally, we can hit Blizzard's PvP API for some basic data.
+# OPTIONAL - We can hit Blizzard's PvP API for some basic data.
 # To toggle this function, comment it out in __main__
 def getPvpApiData():
     try:
@@ -25,7 +26,7 @@ def getPvpApiData():
         print("Error:\n\t" + str(e) + "\n\tClosing application...")
         sys.exit(1)
 
-# Optionally, we can write the results of the API call from the function above to a json file.
+# OPTIONAL - We can write the results of the API call from the function above to a json file.
 def makeFile(data, fileName="whoops.json"):
     try:
         file = open(fileName, "w")
@@ -33,6 +34,14 @@ def makeFile(data, fileName="whoops.json"):
         file.close()
     except IOError as e:
         print("Problem creating pvp-api-data file...\n\t" + str(e) + "\n\tContinuing...")
+
+# Returns a connection to the default database "postgres"
+def postgresConnect(host="localhost", dbname="postgres", user="postgres", password=None):
+    return psycopg2.connect("host='{}' dbname='{}' user='{}'".format(host, dbname, user))
+
+
+
+
 
 
 # Here's where all the magic will happen!
@@ -54,7 +63,7 @@ if __name__ == "__main__":
     # 2v2 Ladder Scraping.
     # UPDATE: This doesn't work. The data we need is loaded after the DOM is finished loading via javascript probably,
     # so we aren't getting the character names/classes etc.
-    twosBytes = req.get("https://worldofwarcraft.com/en-gb/game/pvp/leaderboards/2v2")
+    twosBytes = req.get("https://worldofwarcraft.com/en-us/game/pvp/leaderboards/2v2")
     twosText = twosBytes.text
     twosSoup = soup(html.unescape(twosText), "html.parser").encode("utf-8")
     with open("twosSoup.html", "wb") as twosFile: # Note: MUST open file with "wb"(writebytes) as second arg to avoid encoding issues
@@ -73,11 +82,40 @@ if __name__ == "__main__":
         sys.exit("No driver in \"chromedrivers/\" for detected operating system: " + str(osys))
 
     # This actually DOES work, and the first data we need to save and/or parse is stored in "twosInnerHTML" and then written into "twosInnerHtml.html"
-    browser = webdriver.Chrome("./chromedrivers/" + driver)
-    browser.get("https://worldofwarcraft.com/en-gb/game/pvp/leaderboards/2v2")
-    twosInnerHTML = browser.execute_script("return document.body.innerHTML").encode("utf-8")
-    with open("twosInnerHtml.html", "wb") as twosInnerHtmlFile:
-        twosInnerHtmlFile.write(twosInnerHTML)
-        twosInnerHtmlFile.close()
+    # Changing the if condition to False will turn off scraping. Data will load from file.
+    if True:
+        browser = webdriver.Chrome("./chromedrivers/" + driver)
+        browser.get("https://worldofwarcraft.com/en-us/game/pvp/leaderboards/2v2")
+        twosInnerHTML = browser.execute_script("return document.body.innerHTML").encode("utf-8")
+        with open("twosInnerHtml.html", "wb") as twosInnerHtmlFile:
+            twosInnerHtmlFile.write(twosInnerHTML)
+            twosInnerHtmlFile.close()
+        browser.close()
+    else:
+        with open('twosInnerHtml.html', 'r') as myfile:
+            twosInnerHTML = myfile.read().replace('\n', '')
+ 
+    # Open connection and cursor with PostgresSQL instance
+    conn = postgresConnect(dbname="pyStats", user="pyStats_user", password="pystats")
+    conn.set_session(autocommit=True)
+    curs = conn.cursor()
 
-    # TODO - Get BeautifulSoup to use the html file from above (or just use the variable twosInnerHTML), and start parsing it.
+    # Beautiful, BeautifulSoup. I'm bad at this.
+    twosSoupJs = soup(twosInnerHTML, "html.parser")
+    fourBodyDivs = twosSoupJs('div', attrs={'class': 'SortTable-body'})
+    targetDiv = fourBodyDivs[4]
+    for section in targetDiv:
+        charInfoDiv = section.select('div')[1].a.select('div')
+        charName = charInfoDiv[11].text
+        moreCharInfoList = charInfoDiv[12].text.split(" ")
+        charClass = moreCharInfoList[2]
+        charSpec = moreCharInfoList[1]
+        charPvpDiv = section.select('div')
+        charRating = charPvpDiv[20].text
+        print(charName + " " + charClass + " " + charSpec + " " + charRating)
+        sql = "INSERT INTO ladder2v2 (charname, class, spec, rating) VALUES ('{}','{}','{}',{});".format(charName, charClass, charSpec, charRating)
+        curs.execute(sql)
+    curs.close()
+    conn.close()
+
+    # TODO - Actually parse the interesting stuff
