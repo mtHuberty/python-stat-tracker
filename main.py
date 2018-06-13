c39f5b4a096a5995da54a1f8d7553ac395521408
Matt Huberty <matthewhuberty77@gmail.com>
2018-06-13 18:19:08 -0500

    New DB column working, existing chars will update rating/spec.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import requests as req
 import yaml
 import sys
@@ -37,7 +38,7 @@ def makeFile(data, fileName="whoops.json"):
 
 # Returns a connection to the default database "postgres"
 def postgresConnect(host="localhost", dbname="postgres", user="postgres", password=None):
-    return psycopg2.connect("host='{}' dbname='{}' user='{}'".format(host, dbname, user))
+    return psycopg2.connect("host='{}' dbname='{}' user='{}' password={}".format(host, dbname, user, password))
 
     # TODO - psycopg2 isn't using password at all...strange that it even connects. Need to fix this (with an if password:, else: block)
     # Also, maybe I should find out why the hell it doesn't seem to need a password when I DEFINITELY set one on pgAdmin.
@@ -52,6 +53,7 @@ if __name__ == "__main__":
     # Open config file and initialize variables from it. No important variables yet.
     cfg = loadConfig()
     pvpApiFile = cfg["filenames"]["pvp-api-file"] + ".json"
+    browserType = cfg["browser-type"]
 
     # Attempt an HTTP get request to the Blizzard PvP (en_US) API for top player names/classes/specs.
     # TODO - Possibly deprecate this and use scraping only (without calling API)
@@ -75,18 +77,22 @@ if __name__ == "__main__":
     # Detect OS of the platform the app is running on and use appropriate chromedriver
     osys = sys.platform
     if osys.startswith("win"):
-        print("Windows OS detected...using windows chromedriver")
-        driver = "chromedriver.exe"
+        print("Windows OS detected...using Windows " + browserType + " driver")
+        driverPath = "./browserdrivers/" + browserType + "_drivers/win_" + browserType + "_driver.exe"
     elif osys.startswith("darwin"):
-        print("Mac OS detected...using Mac chromedriver")
-        driver = "mac_chromedriver"
+        print("Mac OS detected...using Mac " + browserType + " driver")
+        driverPath = "./browserdrivers/" + browserType + "_drivers/mac_" + browserType + "_driver"
+    elif osys.startswith("linux"):
+        print("Linux OS detected...using Mac " + browserType + " driver")
+        driverPath = "./browserdrivers/" + browserType + "_drivers/linux_" + browserType + "_driver"
     else:
-        sys.exit("No driver in \"chromedrivers/\" for detected operating system: " + str(osys))
+        sys.exit("No driver in \"browserdrivers/\" for detected operating system: " + str(osys))
 
     # This actually DOES work, and the first data we need to save and/or parse is stored in "twosInnerHTML" and then written into "twosInnerHtml.html"
     # Changing the if condition to False will turn off scraping. Data will load from file.
+    browser = None
     if True:
-        browser = webdriver.Chrome("./chromedrivers/" + driver)
+        browser = webdriver.Chrome(driverPath)
         browser.get("https://worldofwarcraft.com/en-us/game/pvp/leaderboards/2v2")
         twosInnerHTML = browser.execute_script("return document.body.innerHTML").encode("utf-8")
         with open("twosInnerHtml.html", "wb") as twosInnerHtmlFile:
@@ -102,7 +108,7 @@ if __name__ == "__main__":
     conn.set_session(autocommit=True)
     curs = conn.cursor()
 
-    # Beautiful, BeautifulSoup. I'm bad at this.
+    # Beautiful, BeautifulSoup. I'm bad at this. I don't know what's going on. Trial and error FTW
     twosSoupJs = soup(twosInnerHTML, "html.parser")
     fourBodyDivs = twosSoupJs('div', attrs={'class': 'SortTable-body'})
     targetDiv = fourBodyDivs[4]
@@ -113,11 +119,18 @@ if __name__ == "__main__":
         charClass = moreCharInfoList[2]
         charSpec = moreCharInfoList[1]
         charPvpDiv = section.select('div')
+        charRealm = charPvpDiv[15].text
         charRating = charPvpDiv[20].text
-        print(charName + " " + charClass + " " + charSpec + " " + charRating)
-        sql = "INSERT INTO ladder2v2 (charname, class, spec, rating) VALUES ('{}','{}','{}',{});".format(charName, charClass, charSpec, charRating)
-        curs.execute(sql)
+        print(charName + " " + charRealm + " " + charClass + " " + charSpec + " " + charRating)
+        sql="""
+            INSERT INTO ladder2v2 (charname, realm, class, spec, rating)
+                VALUES (%(charName)s, %(realm)s, %(class)s, %(spec)s, %(rating)s)
+                ON CONFLICT ON CONSTRAINT unique_char
+                DO UPDATE SET spec = %(spec)s, rating = %(rating)s;
+            """
+        #.format(charName, charRealm, charClass, charSpec, charRating)
+        curs.execute(sql, {'charName': charName, 'realm': charRealm, 'class': charClass, 'spec': charSpec, 'rating': charRating})
     curs.close()
     conn.close()
-
+    browser.find_element(By.CSS_SELECTOR, '[data-text="Next"]')
     # TODO - Actually parse the interesting stuff
